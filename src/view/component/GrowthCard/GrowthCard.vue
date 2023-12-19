fc10f3c365d6932c0ae391dc213d9deccba88ce7
supersux <scx122188@gmail.com>
2023-12-20 01:46:54 +0800

    [feature-0001] 添加培养计划卡片

---
+++ src/view/component/GrowthCard/GrowthCard.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="card" v-bind:class="[isMultiple ? shadowMultiple : shadowSingle]">
+        <div class="header">
+            <img class="icon" :src="icon" alt="" />
+            <span class="name">{{ name }}</span>
+        </div>
+        <div class="more">
+            <capsule />
+        </div>
+        
+        <div class="content">
+            <div class="content-column" v-for="item in [[1, 2, 3], [1, 3]]">
+                <div v-for="childItem in item">
+                    <img src="">
+                    <span>
+                        <span>大英雄的经验</span>
+                        <span>x</span>
+                        <span>{{ childItem }}</span>
+                        <span>/</span>
+                        <span>2</span>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="less">
+@import "./GrowthCard.less";
+</style>
+
+<script setup lang="ts">
+import capsule from "../capsule/index.vue"
+console.log(111)
+// 材料类型定义
+// interface Metarials {
+//     name: String,
+//     icon?: string,
+//     need: number,
+//     have?: number
+// }
+
+const props = defineProps({
+    icon: String,
+    name: String,
+    metarials: Array
+})
+const emit = defineEmits(['change', 'delete'])
+const isMultiple = false
+const shadowSingle = 'card-single'
+const shadowMultiple = 'card-multiple'
+
+</script>
---
+++ src/view/component/component.d.ts
@@ -0,0 +1,7 @@
+type GrowthMaterials = {
+    // materials name
+    name: string
+    img?: string
+    needCounts: number
+    haveCounts?: number
+}
